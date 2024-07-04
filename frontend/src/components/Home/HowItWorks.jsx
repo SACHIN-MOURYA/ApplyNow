2e0256e999791cab8517680c11b853890b845b2b
Prateek Thakur <prateek.2022ca069@mnnit.ac.in>
2024-07-05 01:45:21 +0530

    added section HeroaSection HowItWorks Popular Categories and PopularCompanies in home page

--- frontend/src/components/Home/HowItWorks.jsx
+++ frontend/src/components/Home/HowItWorks.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+import { MdFindInPage } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
 
 const HowItWorks = () => {
   return (
-   <></>
-  )
-}
+    <>
+      <div className="howitworks">
+        <div className="container">
+          <h3>How It Works?</h3>
+          <div className="banner">
+            <div className="card">
+              <FaUserPlus />
+              <p>Create Account</p>
+              <p>
+                Create your account on ApplyNow.
+              </p>
+            </div>
+            <div className="card">
+              <MdFindInPage />
+              <p>Find a Job/Post a Job</p>
+              <p>
+                Search for a job of your interest
+                or
+                Post a job.
+              </p>
+            </div>
+            <div className="card">
+              <IoMdSend />
+              <p>Apply For Job/Recruit Suitable Candidates</p>
+              <p>
+                Apply for job or Recruit suitable cadidate
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default HowItWorks
+export default HowItWorks;
